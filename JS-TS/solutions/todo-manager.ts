a8f7091cf7d8e79592b8b460bbc58312db21db88
Kamil Machnik <kamil.machnik@itx.group>
2025-10-25 18:03:12 +0200

    feat(JS-TS): Add ToDoManager facade with init, add, complete, and list methods

--- JS-TS/solutions/todo-manager.ts
+++ JS-TS/solutions/todo-manager.ts
@@ -1,23 +1,31 @@
-import { TodoService } from './todo-service';
-import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import { TodoService } from "./todo-service";
+import { TodoApi } from "./todo-api";
+import { Todo, TodoStatus } from "./types";
 
 export class ToDoManager {
   private service = new TodoService(new TodoApi());
 
   async init(): Promise<void> {
-    throw new Error('init: not implemented');
+    await this.add("Learn TypeScript");
+    await this.add("Refactor code", "Clean up old functions");
+    await this.add("Write tests");
   }
 
-  async add(title: string, description = ''): Promise<void> {
-    throw new Error('add: not implemented');
+  async add(title: string, description = ""): Promise<void> {
+    await this.service.create(title, description);
   }
 
   async complete(id: number): Promise<void> {
-    throw new Error('complete: not implemented');
+    const todos = await this.service.search("");
+    const todo = todos.find((t) => t.id === id);
+    if (!todo) throw new Error(`Todo with id ${id} not found`);
+    if (todo.status !== TodoStatus.COMPLETED) {
+      await this.service.toggleStatus(id);
+    }
   }
 
   async list(): Promise<Todo[]> {
-    throw new Error('list: not implemented');
+    return this.service.search("");
   }
 }
+
